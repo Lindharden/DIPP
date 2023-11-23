d8c9f42e524f843a4df2781376ea608dc318392c
Daniel <dakj@itu.dk>
2023-11-23 15:11:22 +0100

    measure memory usage

--- perf.sh
+++ perf.sh
@@ -2,16 +2,19 @@
 
 # Path to your binary
 BINARY="./bin/binary"
-
-# Run the binary in the background and get its PID
-$BINARY &
-PID=$!
+echo $BINARY
 
 # Use perf to monitor the CPU usage of the binary
-perf stat -p $PID -e cpu-clock,cache-misses,cache-references &
+perf stat -e cpu-clock,cache-misses,cache-references -a $BINARY
 
-# Use top to monitor the memory usage of the binary
-top -p $PID -b -n 1 | grep $PID
+# Execute the binary file using GNU time and store the output in a variable
+output=$(/usr/bin/time -f "%M" $BINARY 2>&1)
 
-# Wait for the binary to finish
-wait $PID
+# Print the output of the binary file
+echo "$output"
+
+# Extract the peak memory usage from the last line of the output
+peak_memory=$(echo "$output" | tail -n 1)
+
+# Print the peak memory usage in kilobytes
+echo "Peak memory usage: $peak_memory KB"
