1fd1878f6a521c5f036423d21fe255e77a964f16
Daniel <dakj@itu.dk>
2024-03-04 11:58:45 +0100

    Removed hooks
    Co-authored-by: Jeppe Lindhard <jepli@itu.dk>
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>

--- src/hooks.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void hook_onehz(void);
-// void hook_gndreset(void);
-void hook_init_early(void);
-void hook_init(void);
--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <param/param_server.h>
 #include <pthread.h>
-#include "hooks.h"
 #include <vmem/vmem_server.h>
 #include <vmem/vmem_file.h>
 #include "vmem_config.h"
@@ -21,14 +20,6 @@ void * vmem_server_task(void * param) {
 void * router_task(void * param) {
 	while(1) {
 		csp_route_work();
-	}
-	return NULL;
-}
-
-static void * onehz_task(void * param) {
-	while(1) {
-		hook_onehz();
-		sleep(1);
 	}
 	return NULL;
 }
@@ -59,10 +50,6 @@ int main(int argc, char *argv[]){
 	void serial_init(void);
 	serial_init();
 
-	/* Gndwdt
-	gndwdt_init();
-    */
-
 	/* Init CSP with */
 	csp_conf.hostname = HOSTNAME; // HOSTNAME defined in meson_options.txt
 	csp_init();
@@ -78,11 +65,6 @@ int main(int argc, char *argv[]){
     csp_print("Route table\r\n");
     csp_rtable_print();
 
-    //server_start();
-	//kiss_init();
-	hook_init_early();
-	/*csp_rt_init(); ROUTING TABLE?? */
-
 	csp_bind_callback(csp_service_handler, CSP_ANY);
 	csp_bind_callback(param_serve, PARAM_PORT_SERVER);
 
@@ -94,23 +76,9 @@ int main(int argc, char *argv[]){
 	static pthread_t vmem_server_handle;
 	pthread_create(&vmem_server_handle, NULL, &vmem_server_task, NULL);
 
-	static pthread_t onehz_handle;
-	pthread_create(&onehz_handle, NULL, &onehz_task, NULL);
-
-	hook_init();
 	while (1){
         sleep(10*1000); //TODO: Handle kbd interupt
     }
 
-	//pthread_cancel(router_handle);
-	//pthread_cancel(onehz_handle);
-	//pthread_cancel(vmem_server_handle);
-	//ptrhead_cancel(ftp_server_handle);
-
-	//pthread_join(router_handle,NULL);
-	//pthread_join(onehz_handle,NULL);
-	//pthread_join(vmem_server_handle,NULL);
-	//pthread_join(ftp_server_handle);
-	
 	return 0;
 }
