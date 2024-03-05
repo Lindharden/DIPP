4ab791461a41e529628c9ecb504064dec45445f3
Daniel <dakj@itu.dk>
2024-03-05 14:20:19 +0100

    Fixed parameter addresses and changed serial0 paramid
    Co-authored-by: Jeppe Lindhard <jepli@itu.dk>
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>

--- src/serial.c
+++ src/serial.c
@@ -6,7 +6,7 @@
 uint32_t _serial0;
 
 
-#define PARAMID_SERIAL0                      31
+#define PARAMID_SERIAL0 99
 
 PARAM_DEFINE_STATIC_RAM(PARAMID_SERIAL0, serial0, PARAM_TYPE_INT32, -1, 0, PM_HWREG, NULL, "", &_serial0, NULL);
 
