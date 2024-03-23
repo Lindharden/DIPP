464621a2a5703b1425e60ed4f8a251b203f29650
Daniel <dakj@itu.dk>
2024-03-23 14:15:17 +0100

    Param description update
    Co-authored-by: Jeppe Lindhard <jepli@itu.dk>
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>

--- src/include/param/dipp_process_param.h
+++ src/include/param/dipp_process_param.h
@@ -8,6 +8,6 @@
 /* Define a pipeline_run parameter */
 static uint8_t _pipeline_run = 0;
 void callback_run(param_t *param, int index);
-PARAM_DEFINE_STATIC_RAM(PARAMID_PIPELINE_RUN, pipeline_run, PARAM_TYPE_UINT8, -1, 0, PM_CONF, callback_run, NULL, &_pipeline_run, "Set the pipeline to execute the file");
+PARAM_DEFINE_STATIC_RAM(PARAMID_PIPELINE_RUN, pipeline_run, PARAM_TYPE_UINT8, -1, 0, PM_CONF, callback_run, NULL, &_pipeline_run, "Set the pipeline to execute according to the value");
 
 #endif
