970b6956415837120bb43dab2940136c2044b049
nikso-itu <nikso@itu.dk>
2024-03-26 11:56:27 +0100

    changed remote param definition
    Co-authored-by: Daniel Kjellberg <dakj@itu.com>
    Co-authored-by: Jeppe Lindhard <jepli@itu.com>

--- src/include/param/vmem_upload_param.h
+++ src/include/param/vmem_upload_param.h
@@ -10,7 +10,5 @@
 /* Define radio CSP parameters */
 static uint32_t _head = 0;
 static uint32_t _tail = 0;
-PARAM_DEFINE_REMOTE(vmem_upload_head, RADIO_NODE_ID, RADIO_IMAGE_BUFFER_HEAD_ID, PARAM_TYPE_UINT32, -1, 0, PM_CONF, &_head, NULL);
-PARAM_DEFINE_REMOTE(vmem_upload_tail, RADIO_NODE_ID, RADIO_IMAGE_BUFFER_TAIL_ID, PARAM_TYPE_UINT32, -1, 0, PM_CONF, &_tail, NULL);
 
 #endif
--- src/vmem/vmem_upload.c
+++ src/vmem/vmem_upload.c
@@ -1,16 +1,20 @@
 #include <unistd.h>
 #include <vmem/vmem_client.h>
 #include <vmem/vmem_server.h>
+#include <param/param.h>
 #include <param/param_client.h>
 #include "vmem_upload.h"
 #include "vmem_upload_param.h"
 
 static vmem_list_t vmem_radio = {0};
 
+// PARAM_DEFINE_REMOTE_DYNAMIC(RADIO_IMAGE_BUFFER_HEAD_ID, vmem_upload_head, RADIO_NODE_ID, PARAM_TYPE_UINT32, -1, 1, PM_CONF, &_head, NULL);
+PARAM_DEFINE_REMOTE(vmem_upload_head, RADIO_NODE_ID, RADIO_IMAGE_BUFFER_HEAD_ID, PARAM_TYPE_UINT32, -1, 0, PM_CONF, &_head, NULL);
+
 void init_radio()
 {
     /* Find address of image buffer vmem on radio */
-    vmem_radio = vmem_client_find(RADIO_NODE_ID, VMEM_LOCATE_TIMEOUT, 2, VMEM_NAME, sizeof(VMEM_NAME) - 1);
+    vmem_radio = vmem_client_find(RADIO_NODE_ID, VMEM_LOCATE_TIMEOUT, 1, VMEM_NAME, strlen(VMEM_NAME));
 }
 
 void upload(char *data, int len)
@@ -20,8 +24,8 @@ void upload(char *data, int len)
         init_radio();
 
     /* Get head address of radio vmem image buffer to write image data to */
-    if (param_pull_single(&vmem_upload_head, -1, 1, false, RADIO_NODE_ID, UPLOAD_TIMEOUT, 2) < 0)
-        printf("Retrieving parameter value failed\n");
+    if (param_pull_single(&vmem_upload_head, -1, 1, 1, vmem_upload_head.node, UPLOAD_TIMEOUT, 2) < 0)
+        printf("Retrieving parameter value failed\n");     
 
     uint32_t write_address = vmem_radio.vaddr + _head;
 
@@ -31,7 +35,7 @@ void upload(char *data, int len)
 
     /* Update head addres of radio vmem image buffer */
     uint32_t new_head = write_address + len;
-    if (param_push_single(&vmem_upload_head, -1, &new_head, false, RADIO_NODE_ID, UPLOAD_TIMEOUT, 2, true) < 0)
+    if (param_push_single(&vmem_upload_head, -1, &new_head, 1, vmem_upload_head.node, UPLOAD_TIMEOUT, 2, true) < 0)
         printf("Push parameter value failed\n");
     
     printf("  Waiting for flash driver to flush\n");
